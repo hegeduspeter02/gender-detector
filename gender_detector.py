0982b497224a44d09372b0a16819396b2f90e56e
Hegedűs Péter <hegipeti25@gmail.com>
2025-01-24 14:34:18 +0100

    refactor, changed logic

--- gender_detector.py
+++ gender_detector.py
@@ -4,8 +4,8 @@ import pandas as pd
 HIGHEST_FREQUENCY = 'D'
 PREFER_HUNGARIAN_NAMES = True
 
-def add_external_hungarian_names_to_detector(detector, external_names, gender):
-    hungary_index = (detector.COUNTRIES.index('hungary'))
+def add_external_names_to_detector(detector, external_names, gender, country):
+    country_index = (detector.COUNTRIES.index(country))
 
     for name in external_names[0]:
         name = name.lower()
@@ -14,9 +14,11 @@ def add_external_hungarian_names_to_detector(detector, external_names, gender):
 
             # if gender matches the one in the dictionary
             if gender in gender_country_dict:
-                detector._set(name, gender, gender_country_dict[gender][:hungary_index] + HIGHEST_FREQUENCY + gender_country_dict[gender][hungary_index + 1:])
+                detector._set(name, gender, gender_country_dict[gender][:country_index] + HIGHEST_FREQUENCY + gender_country_dict[gender][country_index + 1:])
+            else:
+                detector._set(name, gender, ' ' * country_index + HIGHEST_FREQUENCY + ' ' * (len(detector.COUNTRIES) - country_index))
         else:
-            detector._set(name, gender, ' ' * hungary_index + HIGHEST_FREQUENCY + ' ' * (len(detector.COUNTRIES) - hungary_index))
+            detector._set(name, gender, ' ' * country_index + HIGHEST_FREQUENCY + ' ' * (len(detector.COUNTRIES) - country_index))
 
 def shorten_gender(gender):
     if gender == 'male':
@@ -26,7 +28,7 @@ def shorten_gender(gender):
     else:
         return gender
 
-data_frame = pd.read_csv('names.csv', header=None)
+data_frame = pd.read_csv('names.csv', header=None, encoding='utf-8-sig')
 name_column = data_frame[0].str.strip()
 
 name_type = input(
@@ -51,18 +53,19 @@ elif name_type == 'first':
 else:
     raise ValueError("Invalid input. Please enter 'full' or 'first'.")
 
-external_hungarian_names_female = pd.read_csv('external_hungarian_names_female.csv', header=None)
-external_hungarian_names_male = pd.read_csv('external_hungarian_names_male.csv', header=None)
+external_hungarian_names_female = pd.read_csv('data/external_hungarian_names_female.csv', header=None)
+external_hungarian_names_male = pd.read_csv('data/external_hungarian_names_male.csv', header=None)
 
 detector = gender.Detector(case_sensitive=False)
 
-add_external_hungarian_names_to_detector(detector, external_hungarian_names_female, 'female')
-add_external_hungarian_names_to_detector(detector, external_hungarian_names_male, 'male')
+add_external_names_to_detector(detector, external_hungarian_names_female, 'female', 'hungary')
+add_external_names_to_detector(detector, external_hungarian_names_male, 'male', 'hungary')
 
 if(PREFER_HUNGARIAN_NAMES):
     data_frame['Gender'] = data_frame['First Name'].apply(lambda name: detector.get_gender(name, 'hungary'))
 else:
     data_frame['Gender'] = data_frame['First Name'].apply(detector.get_gender)
+
 data_frame['Gender'] = data_frame['Gender'].apply(shorten_gender)
 
 data_frame.to_csv('names_with_genders.csv', index=False, encoding='utf-8-sig')
